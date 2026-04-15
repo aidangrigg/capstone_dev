620723aeee7d679dc9c8638b83d768c18ab3ceae
Aidan Grigg <aidangrigg02@gmail.com>
2026-04-15 14:17:55 +1000

    Added header bar to biomarker widgets & working on settings dialog

--- processing/brainground/biomarker/bandpower.py
+++ processing/brainground/biomarker/bandpower.py
@@ -4,26 +4,30 @@ from scipy.integrate import simpson
 from biomarker.base import Biomarker
 from biomarker.types import FFT
 
+class BandpowerSettings():
+    channels: list[int] = [0, 1]
+    band: tuple[int, int] = (7.0, 12.0)
+    baseline: float = 100.0
+
 class BandpowerBiomarker(Biomarker):
-    settings = {
-        "channels": [0, 1],
-        "band": (7.0, 12.0),
-        "baseline": 100.0,
-    }
+    settings = BandpowerSettings()
 
     score = 0.0
 
     def __init__(self, id: int):
         super().__init__(id)
 
+    def update_settings(self, new_settings: BandpowerSettings):
+        self.settings = new_settings
+
     def compute(self, buffer: np.ndarray, fft: FFT):
-        idxs = np.logical_and(fft.freqs >= self.settings["band"][0],  fft.freqs <= self.settings["band"][1])
+        idxs = np.logical_and(fft.freqs >= self.settings.band[0],  fft.freqs <= self.settings.band[1])
 
         bp_total = 0.0
-        for channel in self.settings["channels"]:
+        for channel in self.settings.channels:
             bp_total += simpson(fft.psd[channel][idxs], dx=fft.resolution)
 
-        bp = bp_total / len(self.settings["channels"])
+        bp = bp_total / len(self.settings.channels)
 
         # TODO: compute difference from average
-        self.score = bp - self.settings["baseline"]
+        self.score = bp - self.settings.baseline
--- processing/brainground/ui/biomarker/bandpower_widget.py
+++ processing/brainground/ui/biomarker/bandpower_widget.py
@@ -1,8 +1,50 @@
 
+from PySide6.QtWidgets import QDialog, QRadioButton, QGridLayout, QDialogButtonBox, QButtonGroup, QLabel, QVBoxLayout
+
 from pyqtgraph import BarGraphItem, PlotWidget
-from biomarker.bandpower import BandpowerBiomarker
+from biomarker.bandpower import BandpowerBiomarker, BandpowerSettings
 from ui.biomarker.base_widget import BaseBiomarkerWidget
 
+FREQUENCY_BANDS = {
+    "Delta": (0.0, 3.0),
+    "Theta": (3.0, 7.0),
+    "Alpha": (7.0, 12.0),
+    "SMR": (12.0, 16.0),
+    "Beta": (12.0, 30.0),
+    "Gamma": (30.0, 50.0),
+}
+
+class BandpowerSettingsDialog(QDialog):
+    def __init__(self, old_settings: BandpowerSettings):
+        super().__init__()
+
+        self.settings = old_settings
+        self.setWindowTitle("Bandpower Settings")
+
+        QBtn = QDialogButtonBox.Apply | QDialogButtonBox.Cancel
+
+        self.freq_button_box = QButtonGroup()
+        self.freq_button_layout = QGridLayout()
+        i, j = 0, 0
+        for band in FREQUENCY_BANDS.keys():
+            radio_button = QRadioButton(band)
+            self.freq_button_box.addButton(radio_button)
+            self.freq_button_layout.addWidget(radio_button, i, j)
+            i += 1
+            if i >= 2:
+                i = 0
+                j += 1
+
+        self.buttonBox = QDialogButtonBox(QBtn)
+        self.buttonBox.accepted.connect(self.accept)
+        self.buttonBox.rejected.connect(self.reject)
+
+        self.layout = QVBoxLayout()
+        message = QLabel("Something happened, is that OK?")
+        self.layout.addWidget(message)
+        self.layout.addLayout(self.freq_button_layout)
+        self.layout.addWidget(self.buttonBox)
+        self.setLayout(self.layout)
 
 class BandpowerWidget(BaseBiomarkerWidget):
     def __init__(self, node: BandpowerBiomarker):
@@ -11,24 +53,23 @@ class BandpowerWidget(BaseBiomarkerWidget):
         self.node = node
 
         # power plot
-        self.widget = PlotWidget()
+        plot_widget = PlotWidget()
         self.plot: BarGraphItem = BarGraphItem(x=[], height=[], width=0.8)
-        self.widget.showGrid(x=True, y=True, alpha=0.3)
-        self.widget.setLabels(left="Delta")
-        self.widget.setYRange(-100, 100)
-        # self.widget.setLimits(yMin=-1, yMax=1)
-        self.widget.addItem(self.plot)
-        self.layout.addWidget(self.widget)
-
-        # plot_widget = PlotWidget()
-        # self.plot: PlotItem = psd_widget.getPlotItem() # type: ignore
-        # self.plot.showGrid(x=True, y=True, alpha=0.3)
-        # self.plot.setLabels(left="", bottom="frequency (Hz)")
-        # self.plot.getViewBox().setMouseEnabled(x=False, y=False) # type: ignore
-        # self.plot.setLimits(xMin=1, xMax=40, yMin=0, yMax=500) # type: ignore
-        # self.layout.addWidget(plot_widget)
+        plot_widget.showGrid(x=True, y=True, alpha=0.3)
+        plot_widget.setLabels(left="Delta")
+        plot_widget.setYRange(-100, 100)
+        plot_widget.addItem(self.plot)
+        self.layout.addWidget(plot_widget)
 
     def update(self):
         delta = self.node.score
         self.plot.setOpts(x=[0], height=[delta], brushes=('g' if delta > 0 else 'r'))
+
+    def create_settings_dialog(self):
+        dialog = BandpowerSettingsDialog(self.node.settings)
+
+        if dialog.exec_():
+            print("it worked")
+        else:
+            print("it didn't work")
 
