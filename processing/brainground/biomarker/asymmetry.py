35b46b7bb5145f0c666ebce5b47b38ee6d72141f
Aidan Grigg <aidangrigg02@gmail.com>
2026-05-05 13:32:15 +1000

    Relative asymmetry

--- processing/brainground/biomarker/asymmetry.py
+++ processing/brainground/biomarker/asymmetry.py
@@ -10,6 +10,7 @@ class AsymmetrySettings():
     left_channel: int = 0
     right_channel: int = 1
     band: FrequencyBand = FrequencyBand.ALPHA
+    relative: bool = False
 
 class AsymmetryBiomarker(Biomarker):
     settings = AsymmetrySettings()
@@ -22,7 +23,23 @@ class AsymmetryBiomarker(Biomarker):
     def update_settings(self, new_settings: AsymmetrySettings):
         self.settings = new_settings
 
-    def compute(self, buffer: np.ndarray, fft: FFT):
+    def compute_relative_asymmetry(self, fft: FFT) -> None:
+        band_range = self.settings.band.value
+        idxs = np.logical_and(fft.freqs >= band_range[0],  fft.freqs <= band_range[1])
+
+        left_bp = simpson(fft.psd[self.settings.left_channel][idxs], dx=fft.resolution)
+        right_bp = simpson(fft.psd[self.settings.right_channel][idxs], dx=fft.resolution)
+
+        total_left_bp = fft.total_bandpower[self.settings.left_channel]
+        total_right_bp = fft.total_bandpower[self.settings.right_channel]
+
+        rel_left_bp = (left_bp / total_left_bp) * 100.0
+        rel_right_bp = (right_bp / total_right_bp) * 100.0
+
+        self.score = rel_right_bp - rel_left_bp
+        logging.info("New asymmetry score computed for %s, value: %d", self.iden.name(), self.score)
+
+    def compute_absolute_asymmetry(self, fft: FFT) -> None:
         band_range = self.settings.band.value
         idxs = np.logical_and(fft.freqs >= band_range[0],  fft.freqs <= band_range[1])
 
@@ -31,3 +48,9 @@ class AsymmetryBiomarker(Biomarker):
 
         self.score = right_bp - left_bp
         logging.info("New asymmetry score computed for %s, value: %d", self.iden.name(), self.score)
+
+    def compute(self, buffer: np.ndarray, fft: FFT):
+        if self.settings.relative:
+            self.compute_relative_asymmetry(fft)
+        else:
+            self.compute_absolute_asymmetry(fft)
